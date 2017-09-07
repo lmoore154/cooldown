560307a33e15940bf1c237875f1170168f401e62
Laura Moore <rileyle@gmail.com>
2017-09-06 21:16:11 -0400

    Add fallback route, set up react DOM and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
+scope :api do
   resources :reservation_slots
   resources :locations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+  get "*path" => 'application#static'
+
 end
