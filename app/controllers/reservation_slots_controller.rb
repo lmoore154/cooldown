68cf8638148345105966759cc84bac25bb777daf
Laura Moore <rileyle@gmail.com>
2017-09-06 21:40:59 -0400

    Add association and scope to models, build first actions in controller

--- app/controllers/reservation_slots_controller.rb
+++ app/controllers/reservation_slots_controller.rb
@@ -1,2 +1,21 @@
 class ReservationSlotsController < ApplicationController
+
+  def available
+    render json: ReservationSlot.available.includes(:location)
+  end
+
+  def update
+    slot = ReservationSlot.find params[:id]
+    if slot.book! reservation_slot_params
+      render json: slot
+    else
+      render slot.errors.full_messages, status: 400
+    end
+  end
+
+  private
+
+  def reservation_slot_params
+    params.require(:reservation_slot).permit(:booked_by, :phone_number, :email)
+  end
 end
--- app/models/reservation_slot.rb
+++ app/models/reservation_slot.rb
@@ -1,3 +1,5 @@
 class ReservationSlot < ApplicationRecord
   belongs_to :location
+
+  scope :available, -> { where(booked_by: nil) }
 end
